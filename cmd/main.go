555e6775c558930652295188df42eac2cbad8e54
roman.vasilyev <rivasilev@miem.hse.ru>
2024-01-13 11:56:35 +0300

    Obs commands base

--- cmd/main.go
+++ cmd/main.go
@@ -69,6 +69,121 @@ func main() {
 				parser(&cmd)
 				obsManager.DoStopRecord(cmd)
 			}),
+			hubman.WithCommand(obsman.SetCurrentPreviewScene{}, func(command core.SerializedCommand, parser executor.CommandParser) {
+				cmd := obsman.SetCurrentPreviewScene{}
+				parser(&cmd)
+				obsManager.DoSetCurrentPreviewScene(cmd)
+			}),
+			hubman.WithCommand(obsman.CreateScene{}, func(command core.SerializedCommand, parser executor.CommandParser) {
+				cmd := obsman.CreateScene{}
+				parser(&cmd)
+				obsManager.DoCreateScene(cmd)
+			}),
+			hubman.WithCommand(obsman.SetSceneName{}, func(command core.SerializedCommand, parser executor.CommandParser) {
+				cmd := obsman.SetSceneName{}
+				parser(&cmd)
+				obsManager.DoSetSceneName(cmd)
+			}),
+			hubman.WithCommand(obsman.SetSceneSceneTransitionOverride{}, func(command core.SerializedCommand, parser executor.CommandParser) {
+				cmd := obsman.SetSceneSceneTransitionOverride{}
+				parser(&cmd)
+				obsManager.DoSetSceneSceneTransitionOverride(cmd)
+			}),
+			hubman.WithCommand(obsman.SetCurrentSceneTransition{}, func(command core.SerializedCommand, parser executor.CommandParser) {
+				cmd := obsman.SetCurrentSceneTransition{}
+				parser(&cmd)
+				obsManager.DoSetCurrentSceneTransition(cmd)
+			}),
+			hubman.WithCommand(obsman.TriggerStudioModeTransition{}, func(command core.SerializedCommand, parser executor.CommandParser) {
+				cmd := obsman.TriggerStudioModeTransition{}
+				parser(&cmd)
+				obsManager.DoTriggerStudioModeTransition(cmd)
+			}),
+			hubman.WithCommand(obsman.SetCurrentSceneCollection{}, func(command core.SerializedCommand, parser executor.CommandParser) {
+				cmd := obsman.SetCurrentSceneCollection{}
+				parser(&cmd)
+				obsManager.DoSetCurrentSceneCollection(cmd)
+			}),
+			hubman.WithCommand(obsman.SetSceneItemBlendMode{}, func(command core.SerializedCommand, parser executor.CommandParser) {
+				cmd := obsman.SetSceneItemBlendMode{}
+				parser(&cmd)
+				obsManager.DoSetSceneItemBlendMode(cmd)
+			}),
+			hubman.WithCommand(obsman.SetSceneItemTransform{}, func(command core.SerializedCommand, parser executor.CommandParser) {
+				cmd := obsman.SetSceneItemTransform{}
+				parser(&cmd)
+				obsManager.DoSetSceneItemTransform(cmd)
+			}),
+			hubman.WithCommand(obsman.RemoveSceneItem{}, func(command core.SerializedCommand, parser executor.CommandParser) {
+				cmd := obsman.RemoveSceneItem{}
+				parser(&cmd)
+				obsManager.DoRemoveSceneItem(cmd)
+			}),
+			hubman.WithCommand(obsman.ToggleVirtualCam{}, func(command core.SerializedCommand, parser executor.CommandParser) {
+				cmd := obsman.ToggleVirtualCam{}
+				parser(&cmd)
+				obsManager.DoToggleVirtualCam(cmd)
+			}),
+			hubman.WithCommand(obsman.StopVirtualCam{}, func(command core.SerializedCommand, parser executor.CommandParser) {
+				cmd := obsman.StopVirtualCam{}
+				parser(&cmd)
+				obsManager.DoStopVirtualCam(cmd)
+			}),
+			hubman.WithCommand(obsman.StartVirtualCam{}, func(command core.SerializedCommand, parser executor.CommandParser) {
+				cmd := obsman.StartVirtualCam{}
+				parser(&cmd)
+				obsManager.DoStartVirtualCam(cmd)
+			}),
+			hubman.WithCommand(obsman.ToggleStream{}, func(command core.SerializedCommand, parser executor.CommandParser) {
+				cmd := obsman.ToggleStream{}
+				parser(&cmd)
+				obsManager.DoToggleStream(cmd)
+			}),
+			hubman.WithCommand(obsman.StartStream{}, func(command core.SerializedCommand, parser executor.CommandParser) {
+				cmd := obsman.StartStream{}
+				parser(&cmd)
+				obsManager.DoStartStream(cmd)
+			}),
+			hubman.WithCommand(obsman.StopStream{}, func(command core.SerializedCommand, parser executor.CommandParser) {
+				cmd := obsman.StopStream{}
+				parser(&cmd)
+				obsManager.DoStopStream(cmd)
+			}),
+			hubman.WithCommand(obsman.SendStreamCaption{}, func(command core.SerializedCommand, parser executor.CommandParser) {
+				cmd := obsman.SendStreamCaption{}
+				parser(&cmd)
+				obsManager.DoSendStreamCaption(cmd)
+			}),
+			hubman.WithCommand(obsman.PauseRecord{}, func(command core.SerializedCommand, parser executor.CommandParser) {
+				cmd := obsman.PauseRecord{}
+				parser(&cmd)
+				obsManager.DoPauseRecord(cmd)
+			}),
+			hubman.WithCommand(obsman.ResumeRecord{}, func(command core.SerializedCommand, parser executor.CommandParser) {
+				cmd := obsman.ResumeRecord{}
+				parser(&cmd)
+				obsManager.DoResumeRecord(cmd)
+			}),
+			hubman.WithCommand(obsman.StartRecord{}, func(command core.SerializedCommand, parser executor.CommandParser) {
+				cmd := obsman.StartRecord{}
+				parser(&cmd)
+				obsManager.DoStartRecord(cmd)
+			}),
+			hubman.WithCommand(obsman.StopRecord{}, func(command core.SerializedCommand, parser executor.CommandParser) {
+				cmd := obsman.StopRecord{}
+				parser(&cmd)
+				obsManager.DoStopRecord(cmd)
+			}),
+			hubman.WithCommand(obsman.ToggleRecord{}, func(command core.SerializedCommand, parser executor.CommandParser) {
+				cmd := obsman.ToggleRecord{}
+				parser(&cmd)
+				obsManager.DoToggleRecord(cmd)
+			}),
+			hubman.WithCommand(obsman.ToggleRecordPause{}, func(command core.SerializedCommand, parser executor.CommandParser) {
+				cmd := obsman.ToggleRecordPause{}
+				parser(&cmd)
+				obsManager.DoToggleRecordPause(cmd)
+			}),
 		),
 		hubman.WithOnConfigRefresh(func(c core.AgentConfiguration) {
 			<-time.After(time.Second * 1)
